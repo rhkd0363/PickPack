d877a95a0561eb82545760515cf4c6fce4fb05e6
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-22 22:57:01 +0900

    feat: chat-service depoly test

--- chat-service/src/main/java/com/pickpack/chatservice/DeployCheck.java
+++ chat-service/src/main/java/com/pickpack/chatservice/DeployCheck.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdacde52d8d0c54beb879f4988311bb07c575e8763d1e6c8dd52660aa5cc1909
-size 409
+oid sha256:cc04f1875d32064dd82cd17a51d2eb0d08def593a3de704e59a40cb1fa720ce4
+size 414
