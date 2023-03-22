3d5b2793df176562c3b8623e1c983c5e792c8555
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-23 02:50:16 +0900

    front deploy test

--- front/src/pages/pick/main/Main.jsx
+++ front/src/pages/pick/main/Main.jsx
@@ -1,9 +1,3 @@
-const Main = () => {
-  return (
-    <>
-      <div>메인입니다.</div>
-    </>
-  );
-};
-
-export default Main;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f180a941fb9bc86c08c7d0e9a35eeabe7789b9a67e4508b9ad36bb76283a49d
+size 114
