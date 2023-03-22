0267031593afdc0cd45b4e56f30867d6b568da29
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-22 23:32:49 +0900

    feat:flight-service deploy test

--- flight-service/src/main/java/com/pickpack/flightservice/DeployCheck.java
+++ flight-service/src/main/java/com/pickpack/flightservice/DeployCheck.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf5306442768d79ac3c3ccc120feb1b6c1a9ef7153dfe230ec0e954277122b0f
-size 419
+oid sha256:e65cc89e664d93d046be2e2efa129a5488341e6364f74e9911b13fb1b49f752b
+size 420
