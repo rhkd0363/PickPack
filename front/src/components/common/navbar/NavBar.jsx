d640428ec64a8fa76c4259750e40227a826ab69b
신현광 <rhkd0363@gmail.com>
2023-03-22 23:48:42 +0900

    feat: 대륙별 국가 GeoJSon 데이터 추가

--- front/src/components/common/navbar/NavBar.jsx
+++ front/src/components/common/navbar/NavBar.jsx
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6994490321cd1b960d976a5505f60cf294cb9a475c180ce4373c32fbaba2a011
-size 78
+const NavBar = () => {
+  return <div>냅바</div>;
+};
+
+export default NavBar;
