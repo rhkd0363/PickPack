fb3646044204957c1af249414130ab37d5d1b725
김승희 <kanada11416@gmail.com>
2023-03-23 02:18:08 +0900

    chore : flight-service.py

--- 김승희/pjtSpark/flight-service.py
+++ 김승희/pjtSpark/flight-service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3311776b309d31ac87eaf77edc23c79ef17ed8b28b730eaf4b8c2619686e886b
-size 2221
+oid sha256:359be9ea0203f27740be8c05efac34e12cb65489ab52ec181f55993571a21553
+size 2216
