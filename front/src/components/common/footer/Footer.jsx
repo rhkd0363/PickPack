8110117224a194a314cf0b49ec510dad45241c7a
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-23 02:42:30 +0900

    feat: add dockerfile

--- front/src/components/common/footer/Footer.jsx
+++ front/src/components/common/footer/Footer.jsx
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c1db78a9f3e8991872ef1de4f4012396683b2d6aeaf41a26302bcbf95a8f4edd
-size 78
+const Footer = () => {
+  return <div>푸터</div>;
+};
+
+export default Footer;
--- front/src/pages/pick/main/Main.jsx
+++ front/src/pages/pick/main/Main.jsx
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8e08857935f326f3942978188fae512c32d37f77eaeee97d4f19adf5e5e541aa
-size 111
+const Main = () => {
+  return (
+    <>
+      <div>메인입니다.</div>
+    </>
+  );
+};
+
+export default Main;
