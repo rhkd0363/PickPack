8502d11319910e70853657c93f70721c7a10fb75
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-22 13:11:54 +0900

    feat: apigateway configuration

---
+++ apigateway-service/src/main/java/com/pickpack/apigatewayservice/ApigatewayServiceApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:efffabffcece662ae443825eea24dbf0de407b904a5b9a05931726908edc4775
+size 357
---
+++ apigateway-service/src/test/java/com/pickpack/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bc82cdf186d793a9d5c283a4c0e53d2af3430c42c7453f1444de466e395220f
+size 242
