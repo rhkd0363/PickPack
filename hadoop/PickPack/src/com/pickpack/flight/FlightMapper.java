f927ce6b947af1b59876ddd9e2aa838b1ef9821e
mwtgal20 <mwtgal20@gmail.com>
2023-03-23 23:34:58 +0900

    feat : 항공권 Hadoop

---
+++ hadoop/PickPack/src/com/pickpack/flight/FlightMapper.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83a013f07b2164cbd153e409912f308109a996b87ea97173af032ed0cffbe802
+size 4857
---
+++ hadoop/PickPack/src/com/pickpack/flight/FlightReducer.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8fccb0ddf6e53a7b240c7439b7e1278c4df8ecb8619dc637ec22a83b018b3434
+size 508
