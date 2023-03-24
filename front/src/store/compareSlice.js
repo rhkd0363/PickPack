95ff254726353890bf2ff69b4dce5f0fd21c77dd
허윤회 <dbsghl123@naver.com>
2023-03-24 17:58:22 +0900

    [ADD] : 항공권 비교하기 사이드바 추가

---
+++ front/src/store/compareSlice.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c69e310359ba33d18c6c4b8c70df9f7dc1e711214f5f80db0b37066f59780862
+size 1183
--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2267bfc6dcc13cd9187c471ef6272381abfd5b438a098a8ea30de3191948ee83
-size 901
+oid sha256:1d67b230ca9d3a6a8636c9613b31090a0bd297e2fb1f6706936c2f02ca32b30f
+size 986
