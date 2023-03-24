11ee66f358240b7fd57b14222feb4664ddc3ecc6
seunghee <kanada11416@gmail.com>
2023-03-24 17:08:14 +0900

    chore : citylist GET uri 변경

--- item-service/src/main/java/com/pickpack/itemservice/DeployCheck.java
+++ item-service/src/main/java/com/pickpack/itemservice/DeployCheck.java
@@ -1,17 +1,3 @@
-package com.pickpack.itemservice;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/item")
-public class DeployCheck {
-
-    @GetMapping("/check")
-    public String welcomeCheck(){
-        return "Welcome to the pick&pack 🥨 item-service";
-    }
-
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:afaaab9cfa684c0a316a7f3a0c5f16c1607b464655a846f7a00b5d06d1779e8d
+size 425
--- item-service/src/main/java/com/pickpack/itemservice/entity/Category.java
+++ item-service/src/main/java/com/pickpack/itemservice/entity/Category.java
@@ -1,5 +1,3 @@
-package com.pickpack.itemservice.entity;
-
-public enum Category {
-    BUY,SELL, BORROW, RENT;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ffe2133f3f1c5232b7e63857e4b10271a5ec17e3a9b59736e9a9606c578630a1
+size 95
