b0ba9c0a55909db3510f1d80d106eeee69ee084e
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-24 17:57:43 +0900

    fix : item-service datasource yml

--- item-service/src/main/java/com/pickpack/itemservice/service/CityService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/CityService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af89bbcb5c1bc37ca956eff2fc693589426fa939e64f8d428408503e62e7810
-size 634
+oid sha256:f52088428f8f58719f4171ea1ce2fd1b5494ec0eb623c4abe38d4ed00a5a68f8
+size 753
