9e7549c95c49b27739ab794214709bd22907441c
seunghee <kanada11416@gmail.com>
2023-03-24 15:27:55 +0900

    chore : tourist entity 삭제

--- item-service/src/main/java/com/pickpack/itemservice/entity/Continent.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00d2a73992459b16c44262c94cdb52f799deeb8969adc2cea017a385628e3755
-size 122
--- item-service/src/main/java/com/pickpack/itemservice/entity/Tourist.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:38cbc665934be329fa06925d1ca8a968994f31e3288bccbf0a033fea69d0c4e9
-size 795
