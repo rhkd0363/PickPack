6212f42e36fd46bccbee2b8449730678745d3e20
김아린 <mwtgal20@gmail.com>
2023-03-25 23:41:01 +0900

    chore : Ticket, Flight Entity 단방향으로 변경

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66ac19197dac217a43ac2b0d4697935d1443f5c89808dc37626fb4439f88b5a5
-size 914
+oid sha256:482d257a8cfe1aa083fe14803c5c4c4c58c60a60c9136dd161a880e7a5209081
+size 1395
