9c0cb92dfefd0a3a3f79309b2556f6b6dac6d8e0
김아린 <mwtgal20@gmail.com>
2023-03-25 23:39:53 +0900

    chore : TicketRes, TicketDto 변경

--- flight-service/src/main/java/com/pickpack/flightservice/api/response/TicketRes.java
+++ flight-service/src/main/java/com/pickpack/flightservice/api/response/TicketRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0436a20636c9de7429bcc03daff0a78784e28fde4632be6b2b57d503b1e6f3b3
-size 365
+oid sha256:8c580a0ef7725f9fd16f44b2b5c2c45742bcbdeb771206143cdf731f2e10eaa4
+size 443
--- flight-service/src/main/java/com/pickpack/flightservice/dto/flight/TicketDto.java
+++ flight-service/src/main/java/com/pickpack/flightservice/dto/flight/TicketDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a7b4337db244ff93f93b2e67e53b3d79621d0d0e0001e9e2cdf9040170090dc
-size 1175
+oid sha256:75627c5aef7b1074d281a6a8e941eb0614164334577e69639c877f5b9fa516bf
+size 1114
