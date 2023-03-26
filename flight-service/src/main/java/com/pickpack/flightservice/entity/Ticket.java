0ff7b9f12516dc28a8dd0adba6383b612c8bafea
rin-k645 <mwtgal20@gmail.com>
2023-03-26 14:50:59 +0900

    fix : Ticket Entity annotation error

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:482d257a8cfe1aa083fe14803c5c4c4c58c60a60c9136dd161a880e7a5209081
-size 1395
+oid sha256:e38e5148e25829cb363c16416398f342b50b9fcb5c4a0e1de0026f4484148c8c
+size 1492
