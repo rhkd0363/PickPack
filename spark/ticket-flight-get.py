7522020f5a66c2035fcc195b8c77586faed10720
김승희 <kanada11416@gmail.com>
2023-03-26 16:36:22 +0900

    feat : ticket insert, update 및 flight insert

--- spark/ticket-flight-get.py
+++ spark/ticket-flight-get.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbd09aae8aec1c8caf5d1ef31926035595414a85c98219c6638176375b34a6e3
-size 4178
+oid sha256:489115f1c4ea31a77b6b5ec98f5d3d7f140e662782ac399f974f8b09343e294a
+size 8533
