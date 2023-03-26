8debf83f55cfb07f53a8ba598dc6545563829e51
seunghee <kanada11416@gmail.com>
2023-03-26 16:09:44 +0900

    chore : ticket column 명 변경

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93beebffd9d3718547eef5bedcb16ef8a0fe8f52c00eab18283db89d0b7a386f
+oid sha256:a7861ad037b674c7283a9bd63b3ce293fb816741914592e7d0b991e435bc7830
 size 1492
