d296aad76dfdf998668dfbed5d6cd9424349b6cf
rin-k645 <mwtgal20@gmail.com>
2023-03-26 14:52:46 +0900

    chore : TicketRepository 쿼리 오류로 잠시 주석 처리

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepository.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:784f3697438f2da52cc33d4a7ba92fb21f1873d2049faa6f6f6ccf2622dec037
-size 2019
+oid sha256:0fb830cd3a219add98e044e5457d74f1c6e2203a12702143956b2a0deac8d7d5
+size 2088
