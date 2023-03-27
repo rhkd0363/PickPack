bcd1c8720b9e8a4acbff297ea38f295bca401b67
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-27 21:04:32 +0900

    feat : deploy test

--- member-service/src/main/java/com/pickpack/memberservice/DeployCheck.java
+++ member-service/src/main/java/com/pickpack/memberservice/DeployCheck.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62c7d2ebad8693a59b91ccb26733c4c93c4166c674493f680319592501dd1e71
-size 665
+oid sha256:9dd336c1b3a22bd4c8738066c957abdfaa7f0ef0c99a7023f65de2e34d021f83
+size 668
