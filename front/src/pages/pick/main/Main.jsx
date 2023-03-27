c1bd53730d5310f86fe1757c768dd44d8be8a4e5
신현광 <rhkd0363@gmail.com>
2023-03-27 09:12:22 +0900

    feat: 항공권 검색 추가 및 CSS 수정

--- front/src/pages/pick/main/Main.jsx
+++ front/src/pages/pick/main/Main.jsx
@@ -1,14 +1,3 @@
-import WorldMap from "./elements/WorldMap";
-
-const Main = () => {
-  return (
-    <>
-      <div>메인입니다.</div>
-      <div>
-        <WorldMap />
-      </div>
-    </>
-  );
-};
-
-export default Main;
+version https://git-lfs.github.com/spec/v1
+oid sha256:be88b29cd368e0dad778be508a969d97c5b147044e4dd5542f0bb721aa00ade9
+size 1307
