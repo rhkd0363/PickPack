bdc7ac9e8b1b408d9f293e0aba8d2b45b6943178
허윤회 <dbsghl123@naver.com>
2023-03-28 13:14:44 +0900

    feat : 비교하기 사이드바 완료

--- front/src/store/compareSlice.js
+++ front/src/store/compareSlice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c69e310359ba33d18c6c4b8c70df9f7dc1e711214f5f80db0b37066f59780862
-size 1183
+oid sha256:95d2361ec8dda9978827cccd876fe02fd6dc3d7986d9bc9b123692e0124e431c
+size 1340
--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d67b230ca9d3a6a8636c9613b31090a0bd297e2fb1f6706936c2f02ca32b30f
-size 986
+oid sha256:cabaee6845e30e7b0424d06af88f6df260c3ffd6409bbf2a6c0a3c629f9b42c6
+size 985
