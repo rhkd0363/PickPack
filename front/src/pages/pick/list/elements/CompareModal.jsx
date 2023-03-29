9f7a8d7c1a41174749e465c6d5c80f01451f1f91
허윤회 <dbsghl123@naver.com>
2023-03-29 17:56:28 +0900

    feat: 항공권 비교 테이블 추가

--- front/src/pages/pick/list/elements/CompareModal.jsx
+++ front/src/pages/pick/list/elements/CompareModal.jsx
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:94ce3fe606633329700b150eeb316451cfbb36aedc28e9bdb784b50af9b71436
-size 3162
+import { useSelector } from 'react-redux';
+import CompareModalItem from './CompareModalItem';
+export default function CompareModal(){
+    const color = ['#FF0013', 'orange', '#1CC500', '#72A2FF', 'purple'];
+    const compareList = useSelector((state)=>{
+        return state.compare.compareList;
+    });
+    return(
+        <div style={{border: '3px solid red', textAlign: 'center', display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
+            <h2>항공권 비교 테이블</h2>
+            <table style={{borderCollapse: 'collapse'}}>
+                <thead>
+                    <tr>
+                        <th  style={{border: '1px solid black', width: '200px'}}>출발정보</th>
+                        <th  style={{border: '1px solid black', width: '200px'}}>도착정보</th>
+                        <th  style={{border: '1px solid black', width: '200px'}}>경유정보</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>소요시간</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>항공사</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>가격</th>
+                        <th  style={{border: '1px solid black', width: '100px'}}>상세정보</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {compareList.map((compareItem, index) => (
+                       <CompareModalItem key={compareItem.flightId} item={compareItem} color={color[index]}/>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
