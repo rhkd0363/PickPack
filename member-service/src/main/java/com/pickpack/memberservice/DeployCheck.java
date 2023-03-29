4e616c5865f029b2b15d1e5edbe40a64c2a8f3fc
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-29 15:03:54 +0900

    feat : msa config client ¼³Á¤

--- member-service/src/main/java/com/pickpack/memberservice/DeployCheck.java
+++ member-service/src/main/java/com/pickpack/memberservice/DeployCheck.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dd336c1b3a22bd4c8738066c957abdfaa7f0ef0c99a7023f65de2e34d021f83
-size 668
+oid sha256:4aef6a7a8e2bbf42029a46c446ab124d4a09b535335d320ae14514eeb01551c7
+size 819
