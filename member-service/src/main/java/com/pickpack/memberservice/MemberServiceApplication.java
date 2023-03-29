d06d1c01afe90669144d0332683e4018419c10a8
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-29 10:08:28 +0900

    feat : schedule service

--- member-service/src/main/java/com/pickpack/memberservice/MemberServiceApplication.java
+++ member-service/src/main/java/com/pickpack/memberservice/MemberServiceApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd08a73dc0a5b3fd117c47e8a9beb42f30b3162022998cd305ef8686731f81d9
-size 345
+oid sha256:7cc5a9d70deb699da64b68099c6bb0ba3848377752de79eace74b6829415ed8f
+size 430
---
+++ member-service/src/main/java/com/pickpack/memberservice/service/SchedulerService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6deb029759e5608b1ffd526e02d858d71b2d9468bdb4a3e07d60274ae6ca96f
+size 317
