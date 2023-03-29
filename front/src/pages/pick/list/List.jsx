ea29d4a1123ec40a1b10050b02f4c62bdfe9db3c
허윤회 <dbsghl123@naver.com>
2023-03-29 14:28:27 +0900

    feat: 항공권 비교하기 모달 수정

--- front/src/pages/pick/list/List.jsx
+++ front/src/pages/pick/list/List.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f55fec0e34164f164b927c814146231759ca13416e6fb803d3be4cc237d40ad
-size 13892
+oid sha256:ddbd26b293bdf3c45b47cd86a872c6c0bea0294c57c0ca3e720937767ce820bb
+size 13900
