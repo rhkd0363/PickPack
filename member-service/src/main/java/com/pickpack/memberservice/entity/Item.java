4c3fdfa83edd026f4e1969267527b558a61b8922
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-03-29 12:38:40 +0900

    delete : wrong dir

--- member-service/src/main/java/com/pickpack/memberservice/entity/Item.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:60d5396c2b7e7d229b8c7bab38580cf1600a736c4186ba325265c6f3e73740a4
-size 1163
--- member-service/src/main/java/com/pickpack/memberservice/repository/ItemRepositoryCustomImpl.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af730653e46cbabea2c8fcb8be707e041911d1069a405fb2109155654de4b644
-size 3175
