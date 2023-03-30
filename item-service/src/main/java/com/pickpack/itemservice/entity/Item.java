08c772b8a524f24e3fdcfbee8f5e3c353721fdc0
seunghee <kanada11416@gmail.com>
2023-03-30 13:23:49 +0900

    feat : item 거래 완료 api 구현

--- item-service/src/main/java/com/pickpack/itemservice/entity/Item.java
+++ item-service/src/main/java/com/pickpack/itemservice/entity/Item.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef27931e7baf9caa23058ba364a539abad1a0fca517fa4df54d302b158562c09
-size 2274
+oid sha256:88b1cd2a7749db64cebdcaf47817087839787cc9673ff19dcb1db1583ad74043
+size 2340
--- item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d0088c9c9128d6cf4b5324ae7925cae2d3787b77ee58baed785187ceedcc72c
-size 4571
+oid sha256:bb06db2bf2efc58e3f562326f44de4f141e4396deeb1a7d0cc7c365b2961b14f
+size 4865
