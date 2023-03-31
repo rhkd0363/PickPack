a9d4852947c4dc2a835a046852df78b28ff8260c
이상민 <lsms3723@daum.net>
2023-03-31 17:50:54 +0900

    feat: commit push after merge to develop

---
+++ chat-service/src/main/java/com/pickpack/chatservice/dto/ChatPagingReqDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:02b06f87f093c8005f119e265c367fc2a6859e899667db4a9cb7011816169f17
+size 634
---
+++ chat-service/src/main/java/com/pickpack/chatservice/dto/ChatPagingResDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0a731e9183fe3e791a62a3782c4cf1fa6f6cea7f6ea034c2ebd86a39a6dfd97
+size 771
