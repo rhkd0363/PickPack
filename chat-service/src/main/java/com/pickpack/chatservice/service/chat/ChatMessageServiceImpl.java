2e7a7fa6abbe699439aa55e54bf087dc885608f3
이상민 <lsms3723@daum.net>
2023-04-01 00:08:04 +0900

    build : modify redis host and port

--- chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageServiceImpl.java
+++ chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd27a964a50e74f8631a902a6dd4f49bf628053430c9ceb5ea72de106fc928c8
-size 5770
+oid sha256:fd41ab135329ab3ca25b5130611bf95a9c46515532c9a79880eb1dba18629588
+size 5713
