1b878be8d7eaf8a818c2dbb78e3dc3257e20dd64
이상민 <lsms3723@daum.net>
2023-04-02 03:31:11 +0900

    fix : uri modified to connect nginx

--- chat-service/src/main/java/com/pickpack/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/pickpack/chatservice/controller/ChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f246526256f09eb87cf32ae9d3b9da4637ebe319547e1de1e1d9a064c247f2f6
-size 1748
+oid sha256:5e7be9255b7c8ccc1fd1d323a127e74df880872c425514611ea03947de65aba3
+size 1744
