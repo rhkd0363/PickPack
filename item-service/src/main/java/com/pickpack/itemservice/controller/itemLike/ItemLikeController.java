7ef4eea1bf3840350a32bcaf17b7cd2b6168a9ff
seunghee <kanada11416@gmail.com>
2023-04-01 18:56:33 +0900

    chore : item detail 정보에 member가 찜한 여부 추가

--- item-service/src/main/java/com/pickpack/itemservice/controller/itemLike/ItemLikeController.java
+++ item-service/src/main/java/com/pickpack/itemservice/controller/itemLike/ItemLikeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c438be9688b7e530db4b94ad04b2dc657565ad2a3ca35cb0e33b512428f4904b
-size 1465
+oid sha256:3e9de13fb1c973091a61d8af86c2ccc56adb6d776b42bd7b4ba10e6e049303c3
+size 1471
