c941df3e765f0ae3b95b30d1bb449d60ad88d992
김승희 <kanada11416@gmail.com>
2023-04-01 16:26:40 +0900

    feat : 항공권 찜 목록에서 is_change 값 update

--- spark/ticket-like-update.py
+++ spark/ticket-like-update.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b
-size 1
+oid sha256:806b5fde4b3489941456a757bb356d7ee77669e45f283c4398994a4b6c7d41cf
+size 6119
