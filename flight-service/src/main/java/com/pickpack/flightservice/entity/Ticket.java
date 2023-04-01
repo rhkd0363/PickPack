eef626ce01a18a768f0b540f4b020f5e17068935
김아린 <mwtgal20@gmail.com>
2023-04-02 03:18:23 +0900

    fix : Ticket Entity tendency annotation 수정

--- flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
+++ flight-service/src/main/java/com/pickpack/flightservice/entity/Ticket.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d31859f0d5cc39b331a86e4f96ffbc07504d4e7ec04933255919620ac7bbbdd
-size 1007
+oid sha256:8fefd4b43fab0c956a8853a6824126a930d023bec4c9bc6f69585f40d3b3a29c
+size 1145
