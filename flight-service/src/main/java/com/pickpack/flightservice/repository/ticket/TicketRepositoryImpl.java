d41ce24a7ba82e69ca306c240e66353958c23d50
김아린 <mwtgal20@gmail.com>
2023-04-01 22:53:44 +0900

    feat : Ticket 검색 tendency sort 추가

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3668e119c178a50e0cdb5ad901e42b28ef1594a4a71efa7dc7b7cf2aa0b76b45
-size 4072
+oid sha256:f828ef5c79c7faf68a28030371ba1cc5e1a19b41d88f9fb428b6302ae9d3e186
+size 5694
