06a5689c7595e298237725924d0ba980555c9d2b
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-01 17:57:34 +0900

    chore : item cors fix

--- item-service/src/main/java/com/pickpack/itemservice/config/CorsConfig.java
+++ item-service/src/main/java/com/pickpack/itemservice/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:723ebe747d5420d3504b8811ace86fa3044635dea8bd4abed5235607fbc57358
-size 1182
+oid sha256:951c46ba4018c553689c72c3a4f421a4cf4eaa2f3876f301e1d195bdee8d42e8
+size 1165
