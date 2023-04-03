847be01f51174a10ba3b08139957bccc554f91eb
이상민 <lsms3723@daum.net>
2023-04-03 15:04:51 +0900

    fix : 아 cors

--- chat-service/src/main/java/com/pickpack/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/pickpack/chatservice/controller/ChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc0282456b00050fe99eece99ad6292c3f7b282cbfc5c3893c176cbe1e577576
-size 1868
+oid sha256:789ba9e518256fc96467dce90bb888626290583da41232eddec677214cbef189
+size 1872
