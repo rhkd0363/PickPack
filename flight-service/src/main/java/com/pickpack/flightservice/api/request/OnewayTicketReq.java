6aedb010d83b5e9783b4d95b94a4c2263df3aca7
김아린 <mwtgal20@gmail.com>
2023-04-03 21:30:35 +0900

    fix : TicketRepositoryImpl - querydsl leftJoin으로 바꾸기

--- flight-service/src/main/java/com/pickpack/flightservice/api/request/OnewayTicketReq.java
+++ flight-service/src/main/java/com/pickpack/flightservice/api/request/OnewayTicketReq.java
@@ -1,16 +1,3 @@
-package com.pickpack.flightservice.api.request;
-
-import com.pickpack.flightservice.dto.flight.FilterDto;
-import com.pickpack.flightservice.dto.flight.OneWayInfoDto;
-import com.pickpack.flightservice.dto.flight.PageableDto;
-import lombok.*;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
-public class OnewayTicketReq {
-    private long memberId;
-    private OneWayInfoDto info;
-    private FilterDto filter;
-    private PageableDto pageable;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4a003aa9703e83040959ee5c4a1ae8985dababbc597665b1f0ad96b45a2fcb7
+size 466
--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5165a35bb1f2086465a06bc97dbcf6eca980246aa9cb2fff8dc68cf6bb0d78f
-size 5821
+oid sha256:9344a386a855a565b9bef5406526284fd150ec12e1e32ba5334caf0cebf650db
+size 5886
