0c6450784f3a6206bda9940c06bb3f671fec2293
seunghee <kanada11416@gmail.com>
2023-04-03 22:26:50 +0900

    fix : item list 목록에서 imgUrl을 대표 image의 imgUrl로 변경

--- item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c171985b0bc05d5383150afec6f21da8390ef0a447de0ab03a728f2b5cba8ae1
-size 5824
+oid sha256:a9b7631ec1904a32db3cc5204bc5e50dbe9027ef80ef94008533064b7978fe50
+size 6308
