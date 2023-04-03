8efcfc6bbfec9084b1d2ce1ba227fb74d4674571
이상민 <lsms3723@daum.net>
2023-04-03 14:18:05 +0900

    fix : configmodified

--- chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
+++ chat-service/src/main/java/com/pickpack/chatservice/config/WebSockConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6652156bbc02f9ee4cc233a78104985a745eae46f3067cfb89b7d40bf5415eb9
-size 1887
+oid sha256:65462e7ff7edcf872d8d7241a7364c021b7a10fe71c747ca29ad94925fc7f82c
+size 1884
