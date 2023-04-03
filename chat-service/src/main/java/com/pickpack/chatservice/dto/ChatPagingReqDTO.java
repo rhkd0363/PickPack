d6795068c994e8b7e76c23d951d8a129c4946d28
이상민 <lsms3723@daum.net>
2023-04-03 16:19:12 +0900

    fix : jsonserialize 떼주기

--- chat-service/src/main/java/com/pickpack/chatservice/dto/ChatPagingReqDTO.java
+++ chat-service/src/main/java/com/pickpack/chatservice/dto/ChatPagingReqDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff3874c2efead03a9b7ead627a6cf0ec2292cc0155673c112bc4108284a319e1
-size 661
+oid sha256:0bc9d5067129ea504726a55b865201e176d03de58bd1623813048d7c96960e19
+size 665
