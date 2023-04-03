a8ab2ba7dd811eac95efd12cbbfb47ec24fdf493
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-03 17:56:46 +0900

    feat: auth token config

--- member-service/src/main/java/com/pickpack/memberservice/config/SecurityConfig.java
+++ member-service/src/main/java/com/pickpack/memberservice/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5f835f67b6ff551101dd19ca19f97de5ebd1c3ecf9f6ae3c310dd2890b8473b
-size 3727
+oid sha256:6c1b89efbfe7781f07d3e7d2b0b69141484ab4f5d881a651361d762b9741f21f
+size 3783
