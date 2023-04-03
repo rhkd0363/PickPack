9e361d2cfe38f4b3e47b126909cd0cef64f91609
seunghee <kanada11416@gmail.com>
2023-04-03 14:38:10 +0900

    chore : deploy

--- flight-service/src/main/java/com/pickpack/flightservice/api/request/OnewayTicketLikeReq.java
+++ flight-service/src/main/java/com/pickpack/flightservice/api/request/OnewayTicketLikeReq.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:201271d5a11eec511e3ce47ae8cbfab7d7726ad148750d77e1e074bace13cd1b
-size 171
+package com.pickpack.flightservice.api.request;
+
+import lombok.Getter;
+
+@Getter
+public class OnewayTicketLikeReq {
+    private Long ticketId;
+    private Long memberId;
+}
--- flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
+++ flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:003f48f6b5da423237a0c9abed202b3192309b4d2f73805bd76613810c2ea50b
-size 1244
+package com.pickpack.flightservice.controller.tourist;
+
+import com.pickpack.flightservice.api.response.ListRes;
+import com.pickpack.flightservice.entity.Tourist;
+import com.pickpack.flightservice.service.tourist.TouristService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/flight")
+public class TouristController {
+    private final TouristService touristService;
+
+    @GetMapping(value ="/tourist/{continent}", produces="application/json;charset=UTF-8")
+    public ResponseEntity<?> getTouristsByContinent(@PathVariable("continent") String continent){
+        List<Tourist> touristList = touristService.getTouristsByContinent(continent);
+        
+        if(touristList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(new ListRes(touristList), HttpStatus.OK);
+    }
+}
