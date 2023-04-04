a619a05cc023fc091dde26f7896ae31c3f720dbf
이상민 <lsms3723@daum.net>
2023-04-04 13:03:03 +0900

    fix : cron must have 6 expression..

--- chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatRoomServiceImpl.java
+++ chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatRoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e5a67bcb70a4c701fb9e81bdf113c1120c3f21c10f403b553257b7002b5f90
-size 6617
+oid sha256:2184d1bd2c3ca72895fec41db535185f7e4229a0508e4d163a520a6fda80ad8d
+size 6619
