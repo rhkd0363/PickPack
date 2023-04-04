aef36fed04be2065078d4a74d9f3368ba4759824
이상민 <lsms3723@daum.net>
2023-04-05 03:05:02 +0900

    fix : null

--- chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatRoomServiceImpl.java
+++ chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatRoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:715a0ca36472e72c6b003f830eb9f9bcfd322655f22a9c06bfbc1139eeabd7c8
-size 7351
+oid sha256:034ab29463d212a63f25710e278de079a4a6cc36e7d1de2ad0868214fd38c5b1
+size 7367
