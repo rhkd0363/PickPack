a249202979eab360e4d69b6256f1d62eaba54c97
seunghee <kanada11416@gmail.com>
2023-04-04 14:02:02 +0900

    fix : item 등록 시 image가 없는 경우, null 참조 에러

--- item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c7d67a180a6f0200b0ce90dc0734038efe506351e4be511ef304098fa30d369
-size 6195
+oid sha256:25c028a31c895d44548af8b276292ace32d7022f2512e72c39ac03c9b11e7ec2
+size 6211
