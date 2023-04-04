58a6876da58c692d593d97e815b24f89db52dfbc
이상민 <lsms3723@daum.net>
2023-04-05 02:04:55 +0900

    fix : json

--- chat-service/src/main/java/com/pickpack/chatservice/entity/redis/RedisChatMessage.java
+++ chat-service/src/main/java/com/pickpack/chatservice/entity/redis/RedisChatMessage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7ca92da794cab3ff1e80e3fac0b2e4c8da5fd06b879717f5cf2bcaec82b2c1a
-size 1598
+oid sha256:dbc992f22aa9de05785bae8294277bbea1e4ed05d5100278236e09405b74d9a4
+size 1594
