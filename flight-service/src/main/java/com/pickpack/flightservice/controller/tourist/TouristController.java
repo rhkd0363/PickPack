03990d82c4be7d175e71321baf2c042cfa054770
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-05 16:47:15 +0900

    chore: add log for tourist

--- flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
+++ flight-service/src/main/java/com/pickpack/flightservice/controller/tourist/TouristController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e29a8a0472d470707f0c4b154deb9d63b77c07f12921a6d8603c819739e14b8
-size 1253
+oid sha256:cd9ac8b90c3b0c741dff84ff8d67e65518b12500febcc23f7ed7ff50368a66a9
+size 1369
