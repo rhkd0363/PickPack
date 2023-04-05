e7f61b86bb1a5edcaa3fa0d854f1250fe011038f
김아린 <mwtgal20@gmail.com>
2023-04-05 10:33:38 +0900

    fix : TicketList querydsl - 페이징 시 중복 해결

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0d8ad5a2c488a2588422c995ec4c4fde2f17ca88d6ee3bb451a16091ab3c515
-size 6450
+oid sha256:0b1ab21a3a67e78c6bfdd619e7e6f05f4402c643a8564538b2c9b2cb8759078a
+size 6242
