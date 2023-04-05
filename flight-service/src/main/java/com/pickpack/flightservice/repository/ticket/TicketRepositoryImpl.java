6031be439f476a5f462e77139ed95a456b99a347
김아린 <mwtgal20@gmail.com>
2023-04-05 10:08:19 +0900

    fix : TicketList querydsl - 정렬 조건 추가

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eb948913c928d275055e772f127123e8d069c511019d013941e4dd3ca5f98ef
-size 6237
+oid sha256:a0d8ad5a2c488a2588422c995ec4c4fde2f17ca88d6ee3bb451a16091ab3c515
+size 6450
