bb88ed49c4e8dbcbb31883809e0ce18aaef108ea
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-05 16:24:25 +0900

    feat:alarm

--- member-service/src/main/java/com/pickpack/memberservice/firebase2/FcmController.java
+++ member-service/src/main/java/com/pickpack/memberservice/firebase2/FcmController.java
@@ -1,36 +1,3 @@
-package com.pickpack.memberservice.firebase2;
-
-import com.google.firebase.messaging.FirebaseMessagingException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/member")
-public class FcmController {
-
-    private final FirebaseCloudMessageService firebaseCloudMessageService;
-
-    @PostMapping("/fcm")
-    public ResponseEntity<?> pushMessage(@RequestBody RequestDto requestDto) throws IOException, FirebaseMessagingException {
-
-        System.out.println("🎈🎈🎈🎈🎈🎈🎈🎈");
-        System.out.println(requestDto.getTargetToken() + " "
-                + requestDto.getTitle() + " " + requestDto.getBody());
-
-        firebaseCloudMessageService.sendMessageTo(
-                requestDto.getTargetToken(),
-                requestDto.getTitle(),
-                requestDto.getBody());
-
-        return ResponseEntity.ok().body("success");
-//        return new ResponseEntity("success", HttpStatus.OK);
-
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a5fb02fcf6f1d6023d6d5f46bcbf1759ccb9b9a9a5bf8d32270137f7ea33901
+size 1132
