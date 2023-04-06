16bf16394a577fcfaeb8490880dfca6f9c9bb6b3
김아린 <mwtgal20@gmail.com>
2023-04-07 04:03:25 +0900

    fix : 항공권 검색 - 변동폭 정렬 querydsl 수정

--- flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
+++ flight-service/src/main/java/com/pickpack/flightservice/repository/ticket/TicketRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b1ab21a3a67e78c6bfdd619e7e6f05f4402c643a8564538b2c9b2cb8759078a
-size 6242
+oid sha256:6774ed781ffe9a422a5f84a0a745ed67da1bd84eb929d30e29a6e3c5ad4565f9
+size 6865
