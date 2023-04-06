3bd57326cfa107d7701049165cfc12cb247df9d2
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-06 16:20:35 +0900

    feat:

--- flight-service/src/main/java/com/pickpack/flightservice/service/tourist/TouristService.java
+++ flight-service/src/main/java/com/pickpack/flightservice/service/tourist/TouristService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:128e8171aa4b1790701111bae4308efb726b51114d773a8516611bc7e1d3a01d
-size 1169
+oid sha256:e8557c51d2e0374e65ee6b1ba82b3bbc4935752e69774f04df9da8a1d86c5f3c
+size 1171
