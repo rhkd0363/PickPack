4a1922e3a7b3811f679d201ab459f831668f2009
seunghee <kanada11416@gmail.com>
2023-04-06 13:01:37 +0900

    fix : img가 null일 떄, item modify

--- item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:915b03ef0459b2dd0ff8c65cb811dd4afb5d2825f8342f792c05d251cd8c7c78
-size 8046
+oid sha256:8b10eedb4566d038a44d956e6505e68b0192e70c9bd2629058eb643c230e990b
+size 8150
