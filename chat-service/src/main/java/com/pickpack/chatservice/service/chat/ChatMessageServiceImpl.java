607bbc04f89fa9bf1288747c9ea7050b043d2000
이상민 <lsms3723@daum.net>
2023-04-06 10:26:08 +0900

    fix : create message

--- chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageServiceImpl.java
+++ chat-service/src/main/java/com/pickpack/chatservice/service/chat/ChatMessageServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e660403d7aa1d0dd9b443d350c88a745553da82d535a58e1584b6fc4f2e46fd
-size 7309
+oid sha256:c2145bd4918bc0d21af17c62e16ce7dc3675fdb2c3e0eea603a982ad981bee20
+size 7171
