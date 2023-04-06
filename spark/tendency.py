073efde7607b551b4912426df6f82792429817c1
김승희 <kanada11416@gmail.com>
2023-04-07 05:55:44 +0900

    fix : 항공권 구분

--- spark/tendency.py
+++ spark/tendency.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0de23f2d5d38d8eb0f6bafca8167e6b41232f8d06e3d0736e7691bb59b2eaac
-size 9051
+oid sha256:9732d15dd77a5ee517fcebc4a910fabfd7754420b1af85404f2128ecf47bd1d7
+size 6028
