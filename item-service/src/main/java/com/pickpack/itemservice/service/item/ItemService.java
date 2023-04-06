e4669fa956eb3c2107e62b3f9a62f5e8717265e8
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-07 00:04:55 +0900

    chore: city log

--- item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
+++ item-service/src/main/java/com/pickpack/itemservice/service/item/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b10eedb4566d038a44d956e6505e68b0192e70c9bd2629058eb643c230e990b
-size 8150
+oid sha256:fce8d81c26e44f09bf54f11c24496d7a02890807915f1da00639314ecbefb578
+size 8137
